161be840d86ecc5c4e02e5629899171746a89374
PC <Mohaamed.Ibraahim@gmail.com>
2023-10-08 00:21:44 +0300

    Add project files.

---
+++ ServicesMarketPlace/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ServicesMarketPlace.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
